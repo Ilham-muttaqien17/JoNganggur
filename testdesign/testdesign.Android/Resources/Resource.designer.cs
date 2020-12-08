415bd14d1b5b64fa86e68395cca9dd8394490611
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2020-12-08 20:52:47 +0700

    Create Dashboard Design

--- testdesign/testdesign.Android/Resources/Resource.designer.cs
+++ testdesign/testdesign.Android/Resources/Resource.designer.cs
@@ -8394,19 +8394,22 @@ namespace testdesign.Droid
 			public const int notification_tile_bg = 2131165304;
 			
 			// aapt resource value: 0x7F070079
-			public const int notify_panel_notification_icon_bg = 2131165305;
+			public const int Notifikasi = 2131165305;
 			
 			// aapt resource value: 0x7F07007A
-			public const int TeksJonganggur = 2131165306;
+			public const int notify_panel_notification_icon_bg = 2131165306;
 			
 			// aapt resource value: 0x7F07007B
-			public const int tooltip_frame_dark = 2131165307;
+			public const int TeksJonganggur = 2131165307;
 			
 			// aapt resource value: 0x7F07007C
-			public const int tooltip_frame_light = 2131165308;
+			public const int tooltip_frame_dark = 2131165308;
 			
 			// aapt resource value: 0x7F07007D
-			public const int WelcomeImage = 2131165309;
+			public const int tooltip_frame_light = 2131165309;
+			
+			// aapt resource value: 0x7F07007E
+			public const int WelcomeImage = 2131165310;
 			
 			static Drawable()
 			{
